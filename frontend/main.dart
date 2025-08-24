7bae6390fea1551885f2e6dc7ee806d0cf5b2bee
boeing-boy-97 <bansods685@gmail.com>
2025-08-24 13:00:45 +0530

    Add files via upload

--- frontend/main.dart
+++ frontend/main.dart
@@ -1,1 +1,15 @@
-a
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Edutwin Frontend')),
+        body: Center(child: Text('Hello from Flutter Web!')),
+      ),
+    );
+  }
+}
